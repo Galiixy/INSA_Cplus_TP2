ba4b52de538151c68066f8047dea38d91b31e1f2
Galiixy <gaelle.ferreira@insa-lyon.fr>
2020-10-05 12:33:15 +0200

    Update main.cpp
    Ajout de la fonctionnalité add simple
    Reflexion a faire dans add compose
    Recherche a faire

--- Appli.cpp
+++ Appli.cpp
@@ -52,7 +52,8 @@ static void test ( void )
 
   // Création du trajet composée
   // On ajoute les trajets simples dans l'ordre
-  TrajetComposee * TS2 = new TrajetComposee(1,1,TS2_1);TS2->Ajouter(TS2_2);
+  TrajetComposee * TS2 = new TrajetComposee(1,1,TS2_1);
+  TS2->Ajouter(TS2_2);
 
   //TS2->Afficher();cout << " " << "\r\n";
 
@@ -81,16 +82,19 @@ static void test ( void )
 //---------------------------------------------------- Fonctions publiques
 int main ( void )
 {
-   char lecture[100];
+  char lecture[100];
+  char depart [100];
+  char arrivee [100];
+  Catalogue * catalogue = new Catalogue(3);
+
   cout<<"Bienvenue dans le catalogue de trajet : "<<endl;
   cout<<endl<<"--------------------------------------------------------------------------"<<endl;
-    cout<<"Ecrivez ""test"" pour charger le test de l'application"<<endl;
-    cout<<"Ecrivez ""ajout"" pour ajouter un trajet simple ou composé au catalogue"<<endl;
-    cout<<"Ecrivez ""affichage"" pour afficher le contenu du catalogue"<<endl;
-    cout<<"Ecrivez ""recherche"" pour rechercher un trajet dans le catalogue"<<endl;
-    cout<<"Ecrivez ""bye"" pour quitter l'application"<<endl;
-    cout<<"--------------------------------------------------------------------------"<<endl<<endl;
-
+  cout<<"Ecrivez ""test"" pour charger le test de l'application"<<endl;
+  cout<<"Ecrivez ""ajout"" pour ajouter un trajet simple ou composé au catalogue"<<endl;
+  cout<<"Ecrivez ""affichage"" pour afficher le contenu du catalogue"<<endl;
+  cout<<"Ecrivez ""recherche"" pour rechercher un trajet dans le catalogue"<<endl;
+  cout<<"Ecrivez ""bye"" pour quitter l'application"<<endl;
+  cout<<"--------------------------------------------------------------------------"<<endl<<endl;
 
   fscanf(stdin,"%99s",lecture);
   while (strcmp(lecture,"bye")!=0) {
@@ -98,10 +102,32 @@ int main ( void )
       test();
     } else if (strcmp(lecture,"ajout")==0) {
       //add a route
+      cout<<"Ecrivez ""simple"" pour créer un tout nouveau trajet"<<endl;
+      cout<<"Ecrivez ""composé"" pour créer un trajet composé d'autres trajets"<<endl;
+      fscanf(stdin,"%99s",lecture);
+      if (strcmp(lecture,"simple")==0) {
+        cout << endl <<"Ville de Départ : ";
+        fscanf(stdin,"%99s",lecture);
+        strcpy(depart,lecture);
+        cout << endl <<"Ville d'Arrivée : ";
+        fscanf(stdin,"%99s",lecture);
+        strcpy(arrivee,lecture);
+        cout << endl <<"Moyen de transport : ";
+        fscanf(stdin,"%99s",lecture);
+        Trajet * TS = new TrajetSimple(depart,arrivee, lecture) ;
+        catalogue->Ajouter(TS);
+      }else if (strcmp(lecture,"composé")){
+        //
+      }
     } else if (strcmp(lecture,"affichage")==0) {
-      //print catalogue
+      //affichage
+      catalogue->Afficher();
     } else if (strcmp(lecture,"recherche")==0) {
       //research
+      cout << endl <<"Ville de Départ : ";
+      fscanf(stdin,"%99s",lecture);
+      cout << endl <<"Ville d'Arrivée : ";
+      fscanf(stdin,"%99s",lecture);
     } 
     cout<<endl<<"--------------------------------------------------------------------------"<<endl;
     cout<<"Ecrivez ""test"" pour charger le test de l'application"<<endl;
